5121d81ea67a7425cefaab21406ffbcc4713b94f
Jack O'Connor <jacko7973@gmail.com>
2024-03-23 02:25:07 +0000

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+content: ["**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      display: ['Inter', 'system-ui', 'sans-serif'],
+      body: ['Inter', 'system-ui', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
+
